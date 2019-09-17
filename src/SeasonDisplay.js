b02e1083a1038ba0892f63be9cf5e251fb271e3f
Afreen Siraj <afreen.siraj@capco.com>
2019-09-17 17:00:27 +0100

    Creating SeasonDisplay.js and accessing the state using SeasonDisplay

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const getSeason = (lat, month) => {
+
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    return(
+        <div>
+            Season Display
+        </div>
+    );
+}
+
+export default SeasonDisplay; 
