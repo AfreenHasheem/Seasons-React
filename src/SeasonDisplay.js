6330d7095c0aca6727e92a69ae21a471b8401ea9
Afreen Siraj <afreen.siraj@capco.com>
2019-09-18 10:11:29 +0100

    Adding Conditions to find out which hemisphere the latitude lies in

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 
 const getSeason = (lat, month) => {
-
+    if(month > 2 && month < 9){
+        lat > 0 ? 'summer' : 'winter';
+    }
 }
 
 const SeasonDisplay = (props) => {
